c0e29c6f266ef51bb80cf8237dc08135290465a4
Andre Anijärv <andreanijarv@gmail.com>
2020-03-07 15:00:22 +0200

    Liitmise, lahutamise ja korrutamise põhifunktsionaalsus

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 /*
 Programmi struktuur
 
-Pakun välja esialgse idee programmi struktuurist
+Pakun välja idee programmi struktuurist:
 
 class Tehe
     isendiväljad:
@@ -29,9 +29,12 @@ class Main
         main loop
 */
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
 
         Tehe liitmine = new Tehe(Tehtetuup.LIITMINE);
@@ -42,16 +45,73 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         //main loop
-        while(true){
+        while (true) {
 
+            int raskusaste, suvaline = 1;
+            char reziim;
+            boolean vastusOige = true;
+            System.out.print("Sisestage soovitud raskusaste (0 - välju programmist, 1 - ühekohalised, 2 - kahekohalisd, jne (max 6): ");
+            raskusaste = sc.nextInt();
+            if (raskusaste == 0) break;
+            liitmine.setRaskusaste(raskusaste);
+            lahutamine.setRaskusaste(raskusaste);
+            korrutamine.setRaskusaste(raskusaste);
+            jagamine.setRaskusaste(raskusaste);
+            System.out.print("Sisestage soovitud režiim (j - juhutehe, l - liitlahutamine): ");
+            reziim = sc.next().charAt(0);
+            System.out.print("\nVäljumiseks sisestage \"e\"\n");
 
+            while (true) {
+                try {
+                    if (reziim == 'l') {
+                        if (vastusOige) suvaline = loos(2); // valib kumb tehe kuvada
+                        if (suvaline == 1) vastusOige = teostaTehe(liitmine, vastusOige);
+                        else vastusOige = teostaTehe(lahutamine, vastusOige);
 
-
-
-            if (true) {
-                break;
+                    } else if (reziim == 'j') {
+                        if (vastusOige) suvaline = loos(4);
+                        switch (suvaline) {
+                            case 1:
+                                vastusOige = teostaTehe(liitmine, vastusOige);
+                                break;
+                            case 2:
+                                vastusOige = teostaTehe(lahutamine, vastusOige);
+                                break;
+                            case 3:
+                                vastusOige = teostaTehe(korrutamine, vastusOige);
+                                break;
+                            case 4:
+                                vastusOige = teostaTehe(jagamine, vastusOige);
+                                break;
+                        }
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println();
+                    break;
+                }
             }
-        }
 
+        }
+    }
+
+    private static boolean teostaTehe(Tehe tehe, boolean korrektne) throws InputMismatchException {
+        // Antud tehteobjekt ja boolean, mis näitab, kas eelmine teostatud tehe oli õigesti vastatud
+        // Tulemus: Kuvab kasutajale vastamiseks tehte, arvestades eelmise tulemust
+        // Tagastab true kui kasutaja pakutav vastus on õige
+        // Kui eelmine tehe oli vale, kuvab selle uuesti
+        // todo Lisada võimalus õige vastus kuvada, kui kasutaja seda välja ei mõtle
+        Scanner sc = new Scanner(System.in);
+        if (korrektne) tehe.genTehe();
+        System.out.print(tehe.getTehe());
+        if (!tehe.kontrolliVastus(sc.nextDouble())) {
+            System.out.println("Vale vastus!");
+            return false;
+        } else return true;
+    }
+
+    private static int loos(int ulatus) {
+        // Antud: int ulatus (see näitab mitu võimalikku loosi tulemust saab olla)
+        // Tagastab täisarvu lõigust [1, ulatus]
+        return (int) (Math.round(Math.random() * (ulatus - 1) + 1));
     }
 }
--- src/Tehe.java
+++ src/Tehe.java
@@ -1,36 +1,59 @@
 public class Tehe {
     private Tehtetuup tuup;
     private int raskusaste;
-    private int arv1, arv2;
+    private double arv1, arv2;
     private double vastus;
     private String tehe;
 
-    public Tehe(Tehtetuup tuup){
+    public Tehe(Tehtetuup tuup) {
         this.tuup = tuup;
     }
 
-    public void genTehe(){
-        switch (this.tuup){
+    public void genTehe() {
+        // Meetod genereerib tehtetüübile vastava String-kujul tehte ja salvestab selle vastuse
+
+        int alampiir = -(int) Math.pow(10, raskusaste) + 1;
+        int ulempiir = (int) Math.pow(10, raskusaste) - 1;
+
+        this.arv1 = suvalineInt(alampiir, ulempiir);
+        this.arv2 = suvalineInt(alampiir, ulempiir);
+
+        switch (this.tuup) {
             case LIITMINE:
+                this.vastus = this.arv1 + this.arv2;
+                if (arv2 < 0) this.tehe = String.format("%.0f+(%.0f)=", arv1, arv2);
+                else this.tehe = String.format("%.0f+%.0f=", arv1, arv2);
                 break;
             case LAHUTAMINE:
+                this.vastus = this.arv1 - this.arv2;
+                if (arv2 < 0) this.tehe = String.format("%.0f-(%.0f)=", arv1, arv2);
+                else this.tehe = String.format("%.0f-%.0f=", arv1, arv2);
                 break;
             case KORRUTAMINE:
+                this.vastus = this.arv1 * this.arv2;
+                if (arv2 < 0) this.tehe = String.format("%.0f*(%.0f)=", arv1, arv2);
+                else this.tehe = String.format("%.0f*%.0f=", arv1, arv2);
                 break;
             case JAGAMINE:
+                // todo Jagamine. Tehe tuleb genererida nii, et ei tekiks sobimatuid kümnendmurde ja nulliga jagamist
+                this.tehe = "1/1=";
+                this.vastus = 1;
                 break;
             default:
                 System.out.println("Midagi on valesti");
         }
     }
-    public boolean kontrolliVastus(int pakkumine){
-        return pakkumine == (int)this.vastus;
-    }
-    public boolean kontrolliVastus(double pakkumine){
+
+    public boolean kontrolliVastus(double pakkumine) {
         return pakkumine == vastus;
     }
+
     public void setRaskusaste(int raskusaste) {
         this.raskusaste = raskusaste;
+    }
+
+    private static int suvalineInt(int alampiir, int ulempiir) {
+        return (int) (Math.round(Math.random() * (ulempiir - alampiir) + alampiir));
     }
 
     public Tehtetuup getTuup() {
@@ -41,11 +64,11 @@ public class Tehe {
         return raskusaste;
     }
 
-    public int getArv1() {
+    public double getArv1() {
         return arv1;
     }
 
-    public int getArv2() {
+    public double getArv2() {
         return arv2;
     }
 
@@ -57,6 +80,7 @@ public class Tehe {
         return tehe;
     }
 }
-enum Tehtetuup{
+
+enum Tehtetuup {
     LIITMINE, LAHUTAMINE, KORRUTAMINE, JAGAMINE
 }
